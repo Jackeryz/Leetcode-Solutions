cc570c51a89c2e2fc2c0a57bffdf8241a37c58de
Jackson Tom Joseph <166471331+Jackeryz@users.noreply.github.com>
2026-08-06 20:14:16 +0530

    Archive String to Integer (atoi) (python3) [LeetArchive]

---
+++ String to Integer (atoi)/solution.py
@@ -0,0 +1,12 @@
+
+        num = int(res)
+        
+        if f:
+            return 0
+        if res == '':
+
+                break
+            else:
+                res += c
+            if '0' <= c <= '9':
+        for c in s:
