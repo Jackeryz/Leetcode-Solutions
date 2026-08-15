557342804c549c7046b1d0dc1382fa1acc42f74b
Jackson Tom Joseph <166471331+Jackeryz@users.noreply.github.com>
2026-08-15 23:49:29 +0530

    Archive Longest Subsequence With Non (python3) [LeetArchive]

--- Longest Subsequence With Non/solution.py
+++ Longest Subsequence With Non/solution.py
@@ -1,1 +1,7 @@
-c
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        t = nz = 0
+        for n in nums:
+            nz |= n > 0
+            t ^= n
+        return nz * (len(nums) - (not t))
