be98ac911f6dd1a7d2021d25ebf36d0695d8376e
Jackson Tom Joseph <166471331+Jackeryz@users.noreply.github.com>
2026-09-05 16:10:22 +0530

    Archive String to Integer (atoi) (python3) [LeetArchive]

--- String to Integer (atoi)/solution.py
+++ String to Integer (atoi)/solution.py
@@ -1,3 +1,24 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+        if s == '':
+            return 0
+        res = ''
+        f = 0
+        
+        if s[0] == '-':
+            f = 1
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+        for c in s:
+            if '0' <= c <= '9':
+                res += c
+            else:
+                break
+
+        if res == '':
+            return 0
 
         num = int(res)
         
