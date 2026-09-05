dc15e3ee8567f8cf9d5a114dbf2b2bdba913e02b
Jackson Tom Joseph <166471331+Jackeryz@users.noreply.github.com>
2026-09-05 17:21:00 +0530

    Archive String to Integer (atoi) (python3) [LeetArchive]

--- String to Integer (atoi)/solution.py
+++ String to Integer (atoi)/solution.py
@@ -1,32 +1,18 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.lstrip()
-        if s == '':
-            return 0
-        res = ''
-        f = 0
-        
-        if s[0] == '-':
-            f = 1
+        sign = 1
+        num = 0
+
+        if s and s[0] in '+-':
+            if s[0] == '-':
+                sign = -1
             s = s[1:]
-        elif s[0] == '+':
-            s = s[1:]
+
         for c in s:
-            if '0' <= c <= '9':
-                res += c
-            else:
+            if not c.isdigit():
                 break
+            num = num * 10 + ord(c) - ord('0')
 
-        if res == '':
-            return 0
-
-        num = int(res)
-        
-        if f:
-            num = -num
-        
-        if num < -2**(31):
-            num = -2**(31)
-        elif num > 2**(31)-1:
-            num = 2**(31)-1
-        return num
+        num *= sign
+        return max(-2**31, min(num, 2**31 - 1))
