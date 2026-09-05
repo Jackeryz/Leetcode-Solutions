8dec5ee12545f9b82ac1c29fb56bfd27b0c33e0d
Jackson Tom Joseph <166471331+Jackeryz@users.noreply.github.com>
2026-09-05 15:48:03 +0530

    Archive Reverse Nodes in k (python3) [LeetArchive]

---
+++ Reverse Nodes in k/solution.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        curr = head
+        for _ in range(k):
+            if not curr: return head
+            curr = curr.next
+        prev = None
