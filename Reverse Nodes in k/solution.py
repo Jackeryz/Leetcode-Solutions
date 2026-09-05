7993e330651a6ec7b4e02c8f587d29a585339c51
Jackson Tom Joseph <166471331+Jackeryz@users.noreply.github.com>
2026-09-05 16:09:19 +0530

    Archive Reverse Nodes in k (python3) [LeetArchive]

--- Reverse Nodes in k/solution.py
+++ Reverse Nodes in k/solution.py
@@ -10,3 +10,11 @@ class Solution:
             if not curr: return head
             curr = curr.next
         prev = None
+        curr = head
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        head.next = self.reverseKGroup(curr,k)
+        return prev
